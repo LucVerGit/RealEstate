749fa56ce909712f16c1e7272dfcd45fbf5ba4b1
byebyebabayaga <lucasverrier1@gmail.com>
2021-03-23 23:53:29 +0100

    Update MainActivity.kt

--- app/src/main/java/fr/LucasVerrier/realestatemanager/view/activity/MainActivity.kt
+++ app/src/main/java/fr/LucasVerrier/realestatemanager/view/activity/MainActivity.kt
@@ -1,11 +1,12 @@
 package fr.LucasVerrier.realestatemanager.view.activity
 
+import android.support.v7.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
 
 
-    /
+    //startup
 
 }
